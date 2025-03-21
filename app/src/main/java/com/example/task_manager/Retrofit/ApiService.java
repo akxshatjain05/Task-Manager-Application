8c2ac7a404ca255f921fbf86d1520d551e01527f
akxhatjain05 <akshatjain04889@gmail.com>
2025-03-21 19:15:37 +0530

    Retrofit client and service

---
+++ app/src/main/java/com/example/task_manager/Retrofit/ApiService.java
@@ -0,0 +1,13 @@
+package com.example.task_manager.Retrofit;
+
+import com.example.task_manager.model.Task;
+import com.example.task_manager.model.TaskEntity;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import java.util.List;
+
+public interface ApiService {
+    @GET("todos")
+    Call<List<TaskEntity>> getTasks();
+}
